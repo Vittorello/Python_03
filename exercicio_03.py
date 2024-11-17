d5a952c691ca2a92c9704fa9aad41b1477d4ef0c
Igor Camargo Vittorello <igorvittorello@hotmail.com>
2024-11-17 19:03:12 -0300

    Exercicio 03 filtragem de logs por severidade

---
+++ exercicio_03.py
@@ -0,0 +1,5 @@
+# Filtragem de Logs por Severidade
+
+log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+if log ['level'] == 'ERROR':
+    print(log,['message'])
