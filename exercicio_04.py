c6fae630320f7b373bc36464d066f844a6562008
Igor Camargo Vittorello <igorvittorello@hotmail.com>
2024-11-17 20:22:52 -0300

    Exercicio 04 validacao de dados de entrada

---
+++ exercicio_04.py
@@ -0,0 +1,28 @@
+# Validação de Dados de Entrada
+# try:
+#     idade = int(input("Digite sua idade: "))
+
+#     if not 18 <= idade <= 65:
+#         print("A sua idade não está dentro das validações. ")
+#         exit()
+
+#     else:
+#         print("Sua idade é válida.", idade)
+
+# except ValueError:
+#     print("Erro, digite uma idade numérica.")
+#     exit()
+
+try:
+    email = input("Digite seu melhor e-mail: ").strip()
+
+    if "@" not in email or "." not in email:
+        print("Favor inserir um e-mail válido.")
+        exit()
+
+    else :
+        print("Seu e-mail é válido.")
+
+except ValueError:
+    print("Informe um e-mail válido.")
+    exit()
