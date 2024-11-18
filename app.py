fe7a99eb5eeec2fd85f42dca60f179eb3dc33cc3
Igor Camargo Vittorello <igorvittorello@hotmail.com>
2024-11-18 17:52:25 -0300

    Alguns exemplos de 'while','for','def'

---
+++ app.py
@@ -0,0 +1,26 @@
+# WHILE
+
+# i = 1
+
+# while i < 10:
+#     i += 1
+#     print(i)
+
+# FOR
+
+# criancas = ["Igor","Isabela","Isilda","Orlando"]
+# for contagem in criancas:
+#     print(contagem)
+
+# FOR
+
+# nome = "iloooks"
+# for letra in nome:
+#     print(letra)
+
+
+def soma(primeiro, segundo):
+    resultado = primeiro + segundo
+    return resultado
+
+print(soma(2, 3))
