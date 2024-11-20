45266970ca811a24949750f1e0ccbd2e68cb10ad
Igor Camargo Vittorello <igorvittorello@hotmail.com>
2024-11-20 16:05:25 -0300

    Exercicio 13

--- exercicio_13.py
+++ exercicio_13.py
@@ -1,6 +1,6 @@
 pagina_atual = 1
 paginas_totais = 5 
-while pagina_atual <= 5:
+while pagina_atual <= paginas_totais:
     print(f"Está é a {pagina_atual}, de {paginas_totais}")
     pagina_atual +=1
 print("Todas as páginas foram processadas.")
